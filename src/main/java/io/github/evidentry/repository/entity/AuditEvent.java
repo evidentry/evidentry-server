2172f6ef87443fa3fdf95dc85dcb4fba39e96eb4
Edward L. <edyardlyk@gmail.com>
2025-05-02 16:10:47 +0300

    refactor: use collection table
    Signed-off-by: Edward L. <edyardlyk@gmail.com>

--- src/main/java/io/github/evidentry/repository/entity/AuditEvent.java
+++ src/main/java/io/github/evidentry/repository/entity/AuditEvent.java
@@ -3,6 +3,7 @@ package io.github.evidentry.repository.entity;
 import jakarta.persistence.*;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.*;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -49,12 +51,21 @@ public class AuditEvent implements Serializable {
     private String auditorId;
     @Cascade(CascadeType.ALL)
     @Fetch(FetchMode.SUBSELECT)
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "audit_event_id", referencedColumnName = "id")
+    @ElementCollection
+    @CollectionTable(
+            name = "audit_event_changes",
+            joinColumns = @JoinColumn(name = "audit_event_id"),
+            uniqueConstraints = @UniqueConstraint(columnNames = {"audit_event_id", "field"})
+    )
+    @AttributeOverride(name = "field", column = @Column(name = "field", nullable = false))
+    @AttributeOverride(name = "type", column = @Column(name = "type", nullable = false))
+    @AttributeOverride(name = "value", column = @Column(name = "_value"))
     private Set<AuditEventChange> changes;
     @Cascade(CascadeType.ALL)
     @Fetch(FetchMode.SUBSELECT)
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "audit_event_id", referencedColumnName = "id")
-    private Set<AuditEventAttribute> attributes;
+    @ElementCollection
+    @MapKeyColumn(name = "name")
+    @Column(name = "_value", nullable = false)
+    @CollectionTable(name = "audit_event_attributes", joinColumns = @JoinColumn(name = "audit_event_id"))
+    private Map<@NotBlank String, @NotNull String> attributes;
 }
